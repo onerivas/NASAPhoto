6525669dbd23d8d74b84ca06dd1d8da31e4ebf99
Juan Rivas <one.rivas@gmail.com>
2020-08-31 11:53:05 -0700

    working api request for last 31 days

--- NASA-app/app.js
+++ NASA-app/app.js
@@ -1,97 +1,123 @@
 $(() => {
-let clicked = false;
-const $closeBtn = $('#close-modal')
+  let clicked = false;
+  const $closeBtn = $('#close-modal')
   // testing variable for date
   let today = new Date()
-  console.log(today);
+  // console.log(today);
   // console.log(date);
-
-  //function for the day of the month
-  let newDayOfMonth = () => {
-    for (i = 1; i < 20; i++) {
-      today.getDate() - i;
-      // console.log(today.getDate() - i);
-    }
-  }
-  console.log(today.getDate());
-
-  // Getting the photos for all the divs
-  let dateRequest = () => {
-    for (i = 0; i < 5; i++) {
-       console.log(`https://api.nasa.gov/planetary/apod?api_key=czm8n20NzhRSk6GRW6zD1u7FflGU4VqwyoJohXDp&date=
-    ${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()-i}`)
-    }
-  }
-  dateRequest()
-  // newDayOfMonth()
-  // console.log(dateRequest());
-  // function for todays date in the formatt that is needed for the request.
-  let date = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`
-console.log(date);
-  //grabbing modal elements
-  // let image = $(event.currentTarget).attr('id')
-let currentImage = () => {
-  $(event.currentTarget).css('background-image')
-  console.log(currentImage);
-}
-console.log(currentImage);
-
   //opening the Modal
   const openModal = (event) => {
-    $(event.currentTarget).attr('id', 'Modal')
+    let newModalBackground = $(event.currentTarget).css('background-image')
+    console.log(newModalBackground);
     $('#modal').css('display', 'block')
-    $('#modal-image').css('display', 'block').css('background-image', `url(${currentImage})`)
+    $('#modal-image').css('display', 'block').css('background-image', newModalBackground)
     // console.log('clicked');
   }
-  // close Modal
   const closeModal = () => {
     $('#modal').css('display', 'none')
   }
-$closeBtn.on('click', closeModal)
+  $closeBtn.on('click', closeModal)
+  // //function for the day of the month
+  // let newDayOfMonth = () => {
+  //   for (i = 1; i < 31; i++) {
+  //     if (i < 10) {
+  //       console.log('0' + i);
+  //       // '0' + i;
+  //     } else {
+  //       console.log(i);
+  //     }
+  //     // console.log(today.getDate() - i);
+  //   }
+  // }
+  // console.log(newDayOfMonth);
+  // newDayOfMonth()
 
-  $.ajax({
-      url:
-      // dateRequest()
-      // `https://api.nasa.gov/planetary/apod?api_key=czm8n20NzhRSk6GRW6zD1u7FflGU4VqwyoJohXDp&date=${dateRequest()}`
-      `https://api.nasa.gov/planetary/apod?api_key=czm8n20NzhRSk6GRW6zD1u7FflGU4VqwyoJohXDp&date=2019-09-12`
 
-    })
-
-    .then((data) => {
-      const imageUrl = data.url
-      // for (i = 0; i < 20; i++) {
-
+  // Getting the photos for all the divs
+  let dataRequest = () => {
+    for (i = 1; i <= 31; i++) {
+      $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=czm8n20NzhRSk6GRW6zD1u7FflGU4VqwyoJohXDp&date=${today.getFullYear()}-${today.getMonth()+1}-${i}`
+      }).then((data) => {
+        // console.log(i);
+        const explanation = data.explanation
+        const imageUrl = data.url
+        const $spaceHolder = $('<div>').addClass('holder').appendTo('.container')
         const $spaceImg = $('<div>')
           .attr('id', i)
-          .text(data.title)
           .addClass('space-image')
           .css('background-image', `url(${imageUrl})`)
-          .appendTo('.container')
+          .appendTo($spaceHolder)
           .on('click', openModal)
-        console.log(imageUrl);
-      // }
-
-      // console.log(data);
-      $.ajax({
-        url:   `https://api.nasa.gov/planetary/apod?api_key=czm8n20NzhRSk6GRW6zD1u7FflGU4VqwyoJohXDp&date=2019-09-09`
+        const $imageText = $('<div>')
+          .text(data.title).css('color', 'white')
+          .appendTo($spaceHolder)
 
 
       })
-      .then((data) => {
-        const imageUrl = data.url
-        // for (i = 0; i < 20; i++) {
+      //    console.log('https://api.nasa.gov/planetary/apod?api_key=czm8n20NzhRSk6GRW6zD1u7FflGU4VqwyoJohXDp&date='
+      // `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()-i}`)
+    }
+  }
+  dataRequest()
+  // newDayOfMonth()
+  // console.log(dateRequest());
+  // function for todays date in the formatt that is needed for the request.
+  // let date = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`
+  // console.log(date);
+  //grabbing modal elements
+  let currentImage = $(event.currentTarget).css('background-image')
+  console.log(currentImage);
+  // let image = $(event.currentTarget).attr('id')
 
-          const $spaceImg = $('<div>')
-            .attr('id', i)
-            .text(data.title)
-            .addClass('space-image')
-            .css('background-image', `url(${imageUrl})`)
-            .appendTo('.container')
-            .on('click', openModal)
-            console.log(imageUrl);
-      })
+  console.log(`this is the ${currentImage}`);
 
-    })
+
+
+
+  // $.ajax({
+  //     url:
+  //       // dateRequest()
+  //       // `https://api.nasa.gov/planetary/apod?api_key=czm8n20NzhRSk6GRW6zD1u7FflGU4VqwyoJohXDp&date=${dateRequest()}`
+  //       `https://api.nasa.gov/planetary/apod?api_key=czm8n20NzhRSk6GRW6zD1u7FflGU4VqwyoJohXDp&date=2019-09-12`
+  //
+  //   })
+  //
+  //   .then((data) => {
+  //     // for (i = 0; i < 20; i++) {
+  //
+  //     const imageUrl = data.url
+  //     const $spaceImg = $('<div>')
+  //       .attr('id', i)
+  //       .text(data.title).css('color', 'white')
+  //       .addClass('space-image')
+  //       .css('background-image', `url(${imageUrl})`)
+  //       .appendTo('.container')
+  //       .on('click', openModal)
+  //     console.log(imageUrl);
+  //     // }
+  //
+  //   })
+  // // console.log(data);
+  // $.ajax({
+  //     url: `https://api.nasa.gov/planetary/apod?api_key=czm8n20NzhRSk6GRW6zD1u7FflGU4VqwyoJohXDp&date=2019-09-09`
+  //
+  //
+  //   })
+  //   .then((data) => {
+  //     const imageUrl = data.url
+  //     // for (i = 0; i < 20; i++) {
+  //
+  //     const $spaceImg = $('<div>')
+  //       .attr('id', i)
+  //       .text(data.title).css('color', 'white')
+  //       .addClass('space-image')
+  //       .css('background-image', `url(${imageUrl})`)
+  //       .appendTo('.container')
+  //       .on('click', openModal)
+  //     console.log(imageUrl);
+  //   })
+
 
 
 
